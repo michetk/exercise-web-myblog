3d1f328e90ec0a2be89628a9aa1a78773c452d9c
Michel Miguel <Michel Miguel>
2020-03-27 10:47:57 -0300

    Aqui foi criado o ralacionamento 1 p n e n p 1

--- categories/Category.js
+++ categories/Category.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const connection = require('../database/database')
 
-const Category = connecton.define('categories', {
+const Category = connection.define('categories', {
     title: {
         type: Sequelize.STRING,
         allowNull: false
@@ -11,5 +11,7 @@ const Category = connecton.define('categories', {
         allowNull: false
     }
 })
+
+//Category.sync({force: true})
 
 module.exports = Category
