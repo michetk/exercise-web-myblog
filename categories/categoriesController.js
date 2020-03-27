ec394070b35bc471c1b28e7ab30f166c5965afcf
Michel Miguel <Michel Miguel>
2020-03-27 11:51:56 -0300

    Aqui foi desenvolvido a inserção da categoria no db

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('./Category')
+const slugify = require('slugify')
 
 router.get('/categories', (req, res) => {
     res.send('ROUTER CATEGORIES')
@@ -8,5 +10,21 @@ router.get('/categories', (req, res) => {
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/category_new')
 })
+
+router.post('/categories/save', (req, res) => {
+    let title = req.body.title
+    if (title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/')
+        })
+    } else {
+        res.redirect('/admin/categories/new')
+    }
+})
+
+
 
 module.exports = router
